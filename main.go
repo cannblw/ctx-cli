1639271fbcfffba706fd54a7c6198c27c2a7b19b
Edgar Chirivella <edgar.95.12@gmail.com>
2026-06-02 19:41:23 +0100

    Use constructor in main.go, drop cmd.Execute() wrapper

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
-/*
-Copyright © 2026 Edgar Chirivella
-
-*/
+// Copyright © 2026 Edgar Chirivella
 package main
 
 import "github.com/cannblw/ctx-cli/cmd"
 
 func main() {
-	cmd.Execute()
+	rootCmd := cmd.NewRootCmd()
+	if err := rootCmd.Execute(); err != nil {
+		// cobra already prints the error, just exit non-zero
+		// (in future tasks we'll wire real error handling)
+	}
 }
